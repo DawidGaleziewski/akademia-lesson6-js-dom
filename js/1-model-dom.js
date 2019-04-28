784f03e099826c5109cc394c134960a073bf6694
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-04-28 09:28:58 +0200

    basic dom structure

---
+++ js/1-model-dom.js
@@ -0,0 +1,12 @@
+'use strict'
+// window główny obiekt
+console.log(window);
+    // często używane wartości
+console.log(window.innerHeight);
+console.log(window.outerHeight);
+
+// document
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+
